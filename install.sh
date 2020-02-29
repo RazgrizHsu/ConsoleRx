260b35389d870a5fd2546ad951deb95584a6b814
RazgrizHsu <fly@raz.tw>
2020-02-29 20:02:24 +0800

    fix install..

--- install.sh
+++ install.sh
@@ -1,3 +1,10 @@
+Path=~/.CocosCreator/packages/ConsoleRx
+
 mkdir -p ~/.CocosCreator/packages
-rm -rf ~/.CocosCreator/packages/ConsoleRx
-git clone https://github.com/RazgrizHsu/ConsoleRx.git ~/.CocosCreator/packages/ConsoleRx
+rm -rf $Path
+git clone https://github.com/RazgrizHsu/ConsoleRx.git $Path/tmp
+cd $Path
+mv ./tmp/package/* ./
+rm -rf ./tmp
+
+echo "install ConsoleRx done in $Path"
